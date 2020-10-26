738d2318f2cf9e3f825511983e9d2fd31b0df919
cally2k <callum.rafter@gmail.com>
2020-10-26 20:09:57 +0000

    v4

--- scripts/Animations.js
+++ scripts/Animations.js
@@ -13,32 +13,6 @@ intro.addEventListener("animationend", () => {
     }
 })
 
-// NAVBAR
-// Navbar Appear
-const NavBar = document.querySelector("nav");
-
-window.addEventListener("scroll", () => {
-    if (window.pageYOffset > 100) {
-        NavBar.classList.add("nav-active")
-    } else {
-        NavBar.classList.remove("nav-active")
-    }
-})
-
-// Burger Menu
-
-const navMenu = () => {
-    const burger = document.querySelector(".menu-btn");
-    const navList = document.querySelector(".nav-list");
-
-    burger.addEventListener("click", () => {
-        navList.classList.toggle("nav-list-display");
-        burger.classList.toggle("open");
-    });
-}
-
-navMenu();
-
 // AREA ONE
 // Chevron Animation
 document.getElementById("Chevron").classList.add("chevronAnim");
